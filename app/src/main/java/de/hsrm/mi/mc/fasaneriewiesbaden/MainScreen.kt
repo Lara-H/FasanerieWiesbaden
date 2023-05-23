a38322eb5fcbde167caea787f39f4d5e1c227bfe
Lara Heibel <lara.heibel@student.hs-rm.de>
2023-05-23 13:24:15 +0200

    first simple top bar

--- app/src/main/java/de/hsrm/mi/mc/fasaneriewiesbaden/MainScreen.kt
+++ app/src/main/java/de/hsrm/mi/mc/fasaneriewiesbaden/MainScreen.kt
@@ -2,13 +2,19 @@ package de.hsrm.mi.mc.fasaneriewiesbaden
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
@@ -21,10 +27,29 @@ import androidx.navigation.compose.rememberNavController
 fun MainScreen() {
     val navController = rememberNavController()
     Scaffold(
+        topBar = {TopBar()},
         bottomBar = { BottomBar(navController = navController)}
     ) {
         BottomNavGraph(navController = navController)
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+    TopAppBar(
+        title = { Text(
+            text = "HELLO efopgfsaupoi uf32uwfeuoweufoiwejfoiwjfdoifi",
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        ) },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.Default.Menu, "Menu")
+            }
+        },
+
+    )
 }
 
 @Composable

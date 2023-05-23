74397c5ddda692896eebc0b4964f5402cb84cd04
Lara Heibel <lara.heibel@student.hs-rm.de>
2023-05-23 21:49:30 +0200

    fill level screen

--- app/src/main/java/de/hsrm/mi/mc/fasaneriewiesbaden/screens/main/LevelScreen.kt
+++ app/src/main/java/de/hsrm/mi/mc/fasaneriewiesbaden/screens/main/LevelScreen.kt
@@ -1,14 +1,16 @@
 package de.hsrm.mi.mc.fasaneriewiesbaden
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import de.hsrm.mi.mc.fasaneriewiesbaden.ui.theme.spacing
 
@@ -16,20 +18,76 @@ import de.hsrm.mi.mc.fasaneriewiesbaden.ui.theme.spacing
 fun LevelScreen() {
     Column(modifier = Modifier
         .fillMaxSize()
-        .padding(all = MaterialTheme.spacing.medium)){
-        Text(text = "Level")
+        ){
         Row(modifier = Modifier .fillMaxWidth()) {
-            Column(modifier = Modifier .weight(1f)) {
-                Text(text = "Nut")
-            }
-            Column(modifier = Modifier .weight(1f)) {
-                Text(text = "Nut")
-            }
-            Column(modifier = Modifier .weight(1f)) {
-                Text(text = "Nut")
-            }
+            Image(
+                modifier = Modifier .weight(1f) .padding(all = MaterialTheme.spacing.medium),
+                painter = painterResource(id = R.drawable.nut_full),
+                contentDescription = "Nut",
+                contentScale = ContentScale.Fit
+            )
+            Image(
+                modifier = Modifier .weight(1f) .padding(all = MaterialTheme.spacing.medium),
+                painter = painterResource(id = R.drawable.nut_full),
+                contentDescription = "Nut"
+            )
+            Image(
+                modifier = Modifier .weight(1f) .padding(all = MaterialTheme.spacing.medium),
+                painter = painterResource(id = R.drawable.nut),
+                contentDescription = "Nut"
+            )
         }
-
+        Row(modifier = Modifier .fillMaxWidth()) {
+            Image(
+                modifier = Modifier .weight(1f) .padding(all = MaterialTheme.spacing.medium),
+                painter = painterResource(id = R.drawable.nut),
+                contentDescription = "Nut"
+            )
+            Image(
+                modifier = Modifier .weight(1f) .padding(all = MaterialTheme.spacing.medium),
+                painter = painterResource(id = R.drawable.nut),
+                contentDescription = "Nut"
+            )
+            Image(
+                modifier = Modifier .weight(1f) .padding(all = MaterialTheme.spacing.medium),
+                painter = painterResource(id = R.drawable.nut),
+                contentDescription = "Nut"
+            )
+        }
+        Row(modifier = Modifier .fillMaxWidth()) {
+            Image(
+                modifier = Modifier .weight(1f) .padding(all = MaterialTheme.spacing.medium),
+                painter = painterResource(id = R.drawable.nut),
+                contentDescription = "Nut"
+            )
+            Image(
+                modifier = Modifier .weight(1f) .padding(all = MaterialTheme.spacing.medium),
+                painter = painterResource(id = R.drawable.nut),
+                contentDescription = "Nut"
+            )
+            Image(
+                modifier = Modifier .weight(1f) .padding(all = MaterialTheme.spacing.medium),
+                painter = painterResource(id = R.drawable.nut),
+                contentDescription = "Nut"
+            )
+        }
+        Row(modifier = Modifier .fillMaxWidth()) {
+            Image(
+                modifier = Modifier .weight(1f) .padding(all = MaterialTheme.spacing.medium),
+                painter = painterResource(id = R.drawable.nut),
+                contentDescription = "Nut"
+            )
+            Image(
+                modifier = Modifier .weight(1f) .padding(all = MaterialTheme.spacing.medium),
+                painter = painterResource(id = R.drawable.nut),
+                contentDescription = "Nut"
+            )
+            Image(
+                modifier = Modifier .weight(1f) .padding(all = MaterialTheme.spacing.medium),
+                painter = painterResource(id = R.drawable.nut),
+                contentDescription = "Nut"
+            )
+        }
     }
 }
 
